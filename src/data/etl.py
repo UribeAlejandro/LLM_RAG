0d16b4bbd3fb2daf86e6abaa71d299765a708fe0
Alejandro Uribe <jauriberamirez@gmail.com>
2023-12-05 17:06:58 -0300

    Initialize DVC

---
+++ src/data/etl.py
@@ -0,0 +1,13 @@
+def extract():
+    """Extract datasets from source."""
+    pass
+
+
+def transform():
+    """Transform datasets from source."""
+    pass
+
+
+def load():
+    """Load datasets to destination."""
+    pass
