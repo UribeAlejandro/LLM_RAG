32efc83a250e5badedf9fba7583310d2ffc21277
Alejandro Uribe <jauriberamirez@gmail.com>
2023-12-07 19:41:44 -0300

    ETL V2 (#4)
    * Added Helper functions
    * Refactored extract, transform, load functions
    * Added tests
    * Added DVC & GCP in GHA for testing

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,9 @@ CHUNK_SIZE = 1000
 INDEX_NAME = "clementine-loka"
 EMBEDDINGS_METRIC = "cosine"
 EMBEDDINGS_DIM_GPT4ALL = 384
+EMBEDDINGS_MODEL_PATH = "models/all-MiniLM-L6-v2-f16.gguf"
+
+PATH_RAW_FILES = "datasets/raw"
+PATH_TEST_FILES = "datasets/test"
+
+TEST_TEXTS = ["this is the first chunk of text", "then another second chunk of text is here"]
